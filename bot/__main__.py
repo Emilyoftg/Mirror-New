d0dcbecc02a98e144b3ef7cd980029a29cc68d1b
Emily <71280699+Emilyoftg@users.noreply.github.com>
2021-09-11 22:42:54 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -43,25 +43,27 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>ℹ️ Bot Uptime ℹ️</b>\n<b>{currentTime}</b>\n\n' \
-            f'<b>▶️ Start Time ▶️</b>\n<b>{current}</b>\n\n' \
-            f'<b>⚙️ System Usage ⚙️</b>\n' \
-            f'<b>💿 Disk Space:</b> <b>{total}</b>\n' \
-            f'<b>📀 Used:</b> <b>{used}</b>\n' \
-            f'<b>🕊️ Free:</b> <b>{free}</b>\n' \
-            f'<b>💻 CPU:</b> <b>{cpuUsage}%</b>\n' \
-            f'<b>🖥️ RAM:</b> <b>{memory}%</b>\n' \
-            f'<b>💽 DISK:</b> <b>{disk}%</b>\n\n' \
-            f'<b>📊Data Usage📊</b>\n<b>📤 Upload:</b> <b>{sent}</b>\n' \
-            f'<b>📥 Download:</b> <b>{recv}</b>'
+   stats = f"〣 {CHAT_NAME} 〣\n\n" \
+            f'Rᴜɴɴɪɴɢ Sɪɴᴄᴇ : {currentTime}\n' \
+            f'Sᴛᴀʀᴛᴇᴅ Aᴛ : {current}\n\n' \
+            f'<b>DISK INFO</b>\n' \
+            f'<b><i>Total</i></b>: {total}\n' \
+            f'<b><i>Used</i></b>: {used} ~ ' \
+            f'<b><i>Free</i></b>: {free}\n\n' \
+            f'<b>DATA USAGE</b>\n' \
+            f'<b><i>UL</i></b>: {sent} ~ ' \
+            f'<b><i>DL</i></b>: {recv}\n\n' \
+            f'<b>SERVER STATS</b>\n' \
+            f'<b><i>CPU</i></b>: {cpuUsage}%\n' \
+            f'<b><i>RAM</i></b>: {memory}%\n' \
+            f'<b><i>DISK</i></b>: {disk}%\n'
 
     update.effective_message.reply_photo(IMAGE_X, stats, parse_mode=ParseMode.HTML)
 
 
 def start(update, context):
     start_string = f'''
-This bot can mirror all your links to Google Drive!
-Type /{BotCommands.HelpCommand} to get a list of available commands
+<b>I'm Awake Already!</b>\n<b>Haven't Slept Since:</b> <code>{uptime}</code>
 '''
     buttons = button_build.ButtonMaker()
     buttons.buildbutton("Channel", f"{CHANNEL_LINK}")
@@ -71,15 +73,15 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
     uptime = get_readable_time((time.time() - botStartTime))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         if update.message.chat.type == "private" :
-            sendMessage(f"Hey I'm Alive 🙂\nSince: <code>{uptime}</code>", context.bot, update)
+            sendMessage(f"<b>Hei {update.message.chat.first_name}</b>,\n\nWelcome To One Of A {CHAT_NAME} Bot", context.bot, update)
         else :
             sendMarkup(start_string, context.bot, update, reply_markup)
     else :
-        sendMessage(f"Oops! not a Authorized user.", context.bot, update)
+        sendMessage(f"<b>Hei {uname},</b>\n\n<b>If You Want To Use Me</b>\n\n<b>You Have To Join Nick Leech Group</b>\n\n<b><i>NOTE : All The Uploaded Links Will Be Sent Here In Your Private Chat From Now</i></b>", context.bot, update)
 
 
 def restart(update, context):
-    restart_message = sendMessage("🔄️ 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐈𝐍𝐆...", context.bot, update)
+    restart_message = sendMessage("Restarting The Bot {BOT_NO}", context.bot, update)
     LOGGER.info(f'Restarting The Bot...')
     # Save restart message object in order to reply to it after restarting
     with open(".restartmsg", "w") as f:
@@ -177,28 +179,18 @@ def bot_help(update, context):
         sendMessage(help_string, context.bot, update)
 
 botcmds = [
-        (f'{BotCommands.HelpCommand}','Get Detailed Help'),
         (f'{BotCommands.MirrorCommand}', 'Start Mirroring'),
         (f'{BotCommands.TarMirrorCommand}','Start mirroring and upload as .tar'),
         (f'{BotCommands.UnzipMirrorCommand}','Extract files'),
         (f'{BotCommands.ZipMirrorCommand}','Start mirroring and upload as .zip'),
         (f'{BotCommands.CloneCommand}','Copy file/folder to Drive'),
-        (f'{BotCommands.CountCommand}','Count file/folder of Drive link'),
-        (f'{BotCommands.DeleteCommand}','Delete file from Drive'),
         (f'{BotCommands.WatchCommand}','Mirror Youtube-dl support link'),
         (f'{BotCommands.TarWatchCommand}','Mirror Youtube playlist link as .tar'),
         (f'{BotCommands.CancelMirror}','Cancel a task'),
         (f'{BotCommands.CancelAllCommand}','Cancel all tasks'),
         (f'{BotCommands.ListCommand}','Searches files in Drive'),
         (f'{BotCommands.StatusCommand}','Get Mirror Status message'),
-        (f'{BotCommands.StatsCommand}','Bot Usage Stats'),
-        (f'{BotCommands.RestartCommand}','Restart the bot [owner/sudo only]'),
-        (f'{BotCommands.LogCommand}','Get the Bot Log [owner/sudo only]'),
-        (f'{BotCommands.AuthorizeCommand}','Auth chat [owner/sudo only]'),
-        (f'{BotCommands.UnAuthorizeCommand}','Unauth chat [owner/sudo only]'),
-        (f'{BotCommands.UsageCommand}','See dyno [owner/sudo only]'),
-        (f'{BotCommands.AddSudoCommand}','Add sudo [owner/sudo only]'),
-        (f'{BotCommands.RmSudoCommand}','Remove sudo [owner/sudo only]')
+        (f'{BotCommands.StatsCommand}','Bot Usage Stats')
     ]
 
 
