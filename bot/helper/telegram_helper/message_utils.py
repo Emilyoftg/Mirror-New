f34c27e2a30349867d452d979c32642f595d4058
Emily <71280699+Emilyoftg@users.noreply.github.com>
2021-09-12 15:31:30 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -135,7 +135,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"<b>UL: {ulspeed}ps || DL: {dlspeed}ps</b>"
+        msg += f"\n<b>UL: {ulspeed}ps || DL: {dlspeed}ps</b>"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -176,7 +176,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"<b>UL: {ulspeed}ps || DL: {dlspeed}ps</b>"
+        progress += f"\n<b>UL: {ulspeed}ps || DL: {dlspeed}ps</b>"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
